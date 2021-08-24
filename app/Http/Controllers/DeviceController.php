ca11a84f06646e6ca28b25c326f5fcf406bfa387
semih <semih.kislar@turkticaret.net>
2021-08-24 15:19:10 +0300

    subscription completed

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Device;
+use App\Models\Subscription;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
@@ -56,6 +57,8 @@ class DeviceController extends Controller
         $validationRules = [
             'client_token' => 'required|max:36|exists:devices,client_token',
             'reciept' => 'required',
+            'os' => 'required',
+            'app_id' => 'required|exists:applications,id'
         ];
 
         $validator = Validator::make($request->all(), $validationRules);
@@ -70,20 +73,41 @@ class DeviceController extends Controller
                 ]);
             }
 
+            if (isset($validationErrors['app_id']['Exists'])) {
+                return response()->json([
+                    'status' => 'failure',
+                    'error' => 'Application Doesnt Exists',
+                ]);
+            }
+
             return response()->json([
                 'status' => 'failure',
                 'error' => 'Invalid Request',
             ]);
         }
 
-        $recieptData = $request->only(['client_token', 'reciept']);
+        $recieptData = $request->only(['client_token', 'reciept', 'app_id', 'os']);
 
-        $response = Http::post('http://nginx:80/api/google-mock-api', [
+        $requestResponse = Http::post('http://nginx:80/api/google-mock-api', [
             'client_token' => $recieptData['client_token'],
             'reciept' => $recieptData['reciept'],
         ]);
 
-        return $response;
+        $response['status'] = $requestResponse['status'];
+
+        if ($requestResponse['status']) {
+            Subscription::create([
+                'app_id' => $recieptData['app_id'],
+                'client_token' => $recieptData['client_token'],
+                'expire_date' => $requestResponse['expire-date'],
+                'os' => $recieptData['os']
+            ]);
+
+            $response['client_token'] = $recieptData['client_token'];
+            $response['expire_date'] = $requestResponse['expire-date'];
+        }
+
+        return response()->json($response);
     }
 
     private function generateToken()
---
+++ database/migrations/2021_08_24_114726_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('app_id')->index('app_id');
+            $table->string('client_token')->index('client_token');
+            $table->timestamp('expire_date');
+            $table->string('os');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
